823b52464e79f3283791fe86bdf118693af931cd
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2022-02-28 17:31:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,16 @@ function draw(){
   line(pmouseX,pmouseY,mouseX,mouseY);
   }
 }
+function classifyCanvas(){
+  classifier.classify(canvas, gotResult);
+}
+function gotResult(error,results){
+  if(error){
+    console.error(error);
+  }
+console.log(results)
+document.getElementById('label').innerHTML='label:'+results[0].label;
+document.getElementById('confidence').innerHTML='confidence:'+Math.round(results:[0].confidence*100)+'%';
+utterThis=new SpeechSynthesisUtterance(results[0].label);
+synth.speak(utterThis);
+}
